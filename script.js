e3f4f9fdec1c3aea4cf8f31ae5421a9506d8ef21
Sergey Chv <serge.techno@gmail.com>
2023-05-06 14:44:44 +0300

    added smooth scroll in project

--- script.js
+++ script.js
@@ -29,3 +29,56 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// skroll func
+// console.log(window);
+const btnScroll = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+btnScroll.addEventListener('click', () => {
+  // 1 ver.
+  // window.scrollTo({
+  //   left: section1.getBoundingClientRect().left + window.pageXOffset,
+  //   top: section1.getBoundingClientRect().top + window.pageYOffset,
+  //   behavior: 'smooth'
+  // })
+
+  // 2 ver
+  section1.scrollIntoView({behavior: 'smooth'});
+})
+// console.log(btnScroll.getBoundingClientRect());
+
+// всплытие
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// console.log(randomInt(0,255)) ;
+
+function randomColor() {
+  return `rgb(${randomInt(0,255)}, ${randomInt(0,255)}, ${randomInt(0,255)})`;
+}
+
+// console.log(randomColor());
+
+const nav = document.querySelector('.nav');
+const navLinks = document.querySelector('.nav__links');
+const link = document.querySelector('.nav__link');
+
+nav.addEventListener('click', function(e) {
+  this.style.backgroundColor = randomColor();
+  console.log(e.target);
+  console.log(e.currentTarget);
+  console.log(e.currentTarget === this)
+})
+
+navLinks.addEventListener('click', function(e) {
+  this.style.backgroundColor = randomColor();
+})
+
+
+link.addEventListener('click', function(e) {
+  this.style.backgroundColor = randomColor();
+  // остановить всплытие
+  // e.stopPropagation()
+})
+
